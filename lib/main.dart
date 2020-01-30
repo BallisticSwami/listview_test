b0200598946c9efda4261fb9cb9c70eb11cce4cb
BallisticSwami <sbahadkar@rocketmail.com>
2020-01-30 13:55:30 +0530

    v3

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_material_color_picker/flutter_material_color_picker.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MaterialApp(
-      title: 'Class_2',
-      home: MyApp(),
-    ));
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.transparent,
+      statusBarColor: Colors.cyan,
+      systemNavigationBarIconBrightness: Brightness.dark));
+
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Class_2',
+    home: MyApp(),
+  ));
+}
 
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
 
+List<Color> myColors = [
+  Colors.teal,
+  Colors.orangeAccent,
+  Colors.purpleAccent,
+  Colors.blueAccent,
+  Colors.redAccent,
+  Colors.greenAccent,
+  Colors.pinkAccent
+];
+
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     Color addColor;
-
-    List<Color> myColors = [
-      Colors.teal,
-      Colors.orangeAccent,
-      Colors.purpleAccent,
-      Colors.blueAccent,
-      Colors.redAccent,
-      Colors.greenAccent,
-      Colors.pinkAccent
-    ];
 
     void _showDialog() {
       showDialog(
@@ -41,7 +50,7 @@ class _MyAppState extends State<MyApp> {
                         onPressed: () {
                           setState(() {
                             myColors.add(addColor);
-                            print(myColors.length);
+                            Navigator.pop(context);
                           });
                         },
                       )
@@ -58,52 +67,57 @@ class _MyAppState extends State<MyApp> {
           });
     }
 
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        backgroundColor: Colors.cyan,
-        elevation: 9,
-        onPressed: () {
-          setState(() {
-            _showDialog();
-          });
-        },
-      ),
-      appBar: AppBar(
-        title: Text('Hello frens'),
-        backgroundColor: Colors.cyan,
-      ),
-      body: ListView.builder(
-        padding: EdgeInsets.fromLTRB(10, 15, 10, 10),
-        itemCount: myColors.length,
-        itemBuilder: (context, int index) {
-          return Padding(
-            padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-            child: Container(
-              height: size.height * 0.13,
-              child: Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                elevation: 5,
-                color: myColors[index],
-                child: ListTile(
-                  trailing: Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 28, 0, 0),
-                    child: IconButton(
-                      color: Colors.black.withOpacity(0.15),
-                      icon: Icon(Icons.cancel),
-                      iconSize: 50,
-                      onPressed: () {
-                        myColors.removeAt(index);
-                        setState(() {});
-                      },
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          tooltip: 'Add a color',
+          child: Icon(Icons.add),
+          backgroundColor: Colors.cyan,
+          elevation: 9,
+          onPressed: () {
+            setState(() {
+              _showDialog();
+            });
+          },
+        ),
+        appBar: AppBar(
+          title: Text('Hello frens'),
+          backgroundColor: Colors.cyan,
+        ),
+        body: ListView.builder(
+          physics: BouncingScrollPhysics(),
+          padding: EdgeInsets.fromLTRB(10, 15, 10, 10),
+          itemCount: myColors.length,
+          itemBuilder: (context, int index) {
+            return Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+              child: Container(
+                height: size.height * 0.13,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  elevation: 7,
+                  color: myColors[index],
+                  child: Center(
+                    child: ListTile(
+                      trailing: IconButton(
+                        color: Colors.white.withOpacity(0.8),
+                        icon: Icon(Icons.close),
+                        iconSize: 40,
+                        tooltip: 'Remove this color',
+                        onPressed: () {
+                          setState(() {
+                            myColors.removeAt(index);
+                          });
+                        },
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
